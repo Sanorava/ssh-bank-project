7f2a9809c7567f8eafe10f46f941db00cc6ae2fa
Alrxander Titarenko <azekalg@mail.ru>
2024-10-30 16:33:48 +0300

    sort_by_date dunction

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def filter_by_state(data: dict, state='EXECUTED') -> dict:
     """
     Функция фильтрует список словарей по ключу 'state'.
@@ -8,3 +11,16 @@ def filter_by_state(data: dict, state='EXECUTED') -> dict:
     """
     filtered_data = [item for item in data if item.get('state') == state]
     return filtered_data
+
+
+def sort_by_date(data, ascending=False):
+    """
+    Функция сортирует список словарей по ключу date.
+
+    data: Список словарей, содержащих дату в формате 'YYYY-MM-DD'.
+    ascending: Порядок сортировки; если True, сортировка по возрастанию, если False - по убыванию (по умолчанию).
+    return Новый отсортированный список словарей.
+    """
+    # Делаем сортировку на основе ключа 'date'
+    sorted_data = sorted(data, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%d'), reverse=not ascending)
+    return sorted_data
