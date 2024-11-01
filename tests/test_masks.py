8cee7a5e42fef41d9408f32234146fffcf211d66
Alrxander Titarenko <azekalg@mail.ru>
2024-11-01 17:06:20 +0300

    add tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,2 +1,7 @@
 import pytest
 from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
+assert get_mask_card_number('2202344354456556') == '2202 34** **** 5655'
+assert get_mask_account('111111111111111111111') == '**11111'
