33cd230a13b826aa08cd52b85d6efcd0658b43d3
Alrxander Titarenko <azekalg@mail.ru>
2024-11-02 17:13:14 +0300

    correct tests view

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,5 +3,9 @@ from src.masks import get_mask_card_number
 from src.masks import get_mask_account
 
 
-assert get_mask_card_number('2202344354456556') == '2202 34** **** 5655'
-assert get_mask_account('111111111111111111111') == '**11111'
+def test_get_mask_card_number():
+    assert get_mask_card_number('2202344354456556') == '2202 34** **** 5655'
+
+
+def test_get_mask_account():
+    assert get_mask_account('111111111111111111111') == '**11111'
