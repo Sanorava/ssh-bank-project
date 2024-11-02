7bac7b26ff31dcffd7b778faa5ee7eb66df6679a
Alrxander Titarenko <azekalg@mail.ru>
2024-11-02 12:27:23 +0300

    add tests from wiget

--- tests/test_wiget.py
+++ tests/test_wiget.py
@@ -1,1 +1,9 @@
 import pytest
+from src.wiget import mask_account_card
+from src.wiget import get_date
+
+assert mask_account_card('Счёт 11111111111111111111') == 'Счёт **1111'
+assert mask_account_card('Счет 11111111111111111111') == 'Счет **1111'
+assert mask_account_card('Visa Platinum 7000792289606361') == 'Visa Platinum 7000 79** **** 0636'
+assert mask_account_card('Visa Gold 7000792289606361') == 'Visa Gold 7000 79** **** 0636'
+assert mask_account_card('Maestro 7000792289606361') == 'Maestro 7000 79** **** 0636'
