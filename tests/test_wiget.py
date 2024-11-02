269535b62a12e49d055c45ba3cc6459191ab27e8
Alrxander Titarenko <azekalg@mail.ru>
2024-11-02 20:05:58 +0300

    final hope

--- tests/test_wiget.py
+++ tests/test_wiget.py
@@ -17,3 +17,11 @@ def test_mask_account_card_number(account_card):
 def test_get_date(get_data):
     assert get_date("2024-03-11T02:26:18.671407") == get_data
     assert get_date("2024-11-12T11:45:10.671407") == '12.11.2024'
+
+
+@pytest.mark.parametrize('date, expected', [("2023-06-10T02:26:18.671407", '10.06.2023'),
+                                            ("2024-01-19T02:26:18.671407", '19.01.2024')])
+
+
+def test_get_date(date, expected):
+    assert get_date(date) == expected
