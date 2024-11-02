cf0f13ec8e7b736d5e3b85de5b15ea57793a1c64
Alrxander Titarenko <azekalg@mail.ru>
2024-11-02 12:31:56 +0300

    add get_date test

--- tests/test_wiget.py
+++ tests/test_wiget.py
@@ -7,3 +7,6 @@ assert mask_account_card('Счет 11111111111111111111') == 'Счет **1111'
 assert mask_account_card('Visa Platinum 7000792289606361') == 'Visa Platinum 7000 79** **** 0636'
 assert mask_account_card('Visa Gold 7000792289606361') == 'Visa Gold 7000 79** **** 0636'
 assert mask_account_card('Maestro 7000792289606361') == 'Maestro 7000 79** **** 0636'
+
+
+assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
