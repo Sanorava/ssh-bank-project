e31a0f46419ae5a62bd2f50de6a5804f2198e1fb
Alrxander Titarenko <azekalg@mail.ru>
2024-11-04 19:00:10 +0300

    homework 11.1 first try

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,8 +30,3 @@ def get_data():
 def my_list():
     return [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
             {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
-
-
-@pytest.fixture()
-def card_number():
-    return 1, 9999999999999999
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,5 +1,8 @@
 import pytest
-from src.generators import card_number_generator
+from src.generators import random_card_number
 
-def test_card_number_generator(card_number):
-    assert card_number_generator(1, 9999999999999999) == card_number
+def test_card_number_generator():
+    assert next(random_card_number) == '0000 0000 0000 0001'
+    assert next(random_card_number) == '0000 0000 0000 0002'
+    assert next(random_card_number) == '0000 0000 0000 0003'
+    assert next(random_card_number) == '0000 0000 0000 0004'
